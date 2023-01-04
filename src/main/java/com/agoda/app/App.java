67539f604f185e3dace0d568ba89b483f744bd46
Ritwik Goel <ritwik.goel@antrepriz.com>
2023-01-04 14:21:04 +0530

    Fix: Unzip Directory not found

--- src/main/java/com/agoda/app/App.java
+++ src/main/java/com/agoda/app/App.java
@@ -1,6 +1,7 @@
 package com.agoda.app;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import com.agoda.compress.zip.Zip;
 import com.agoda.constants.Commands;
@@ -10,50 +11,54 @@ import com.agoda.utils.FileUtils;
 
 public class App {
 	public static void main(String[] args) throws IOException {
-		if (args[0].equalsIgnoreCase(Commands.COMPRESS.name())) {
-			String inputDir = "/Users/ritwikgoel/Desktop/input.txt";
+		int input;
+		Scanner io= new Scanner(System.in);
+		input=io.nextInt();
+		if (input==0) {
+			String inputDir = "/Users/ritwikgoel/Desktop/hello.txt";
 			String outputDir = "/Users/ritwikgoel/Desktop/";
-			long fileSize = FileUtils.MBtobytes(Double.parseDouble(args[3]));
+			long fileSize = FileUtils.MBtobytes(Double.parseDouble("100"));
 
 			// Currently using only ZIP compression
 			CompressionAlgo compressionAlgorithm = CompressionAlgo.ZIP;
 			switch (compressionAlgorithm) {
-			case ZIP:
-				Zip compressor = new Zip();
-				System.out.println("Starting to compress " + inputDir + " to " + outputDir + " using "
-						+ CompressionAlgo.ZIP.name() + " compression");
-				long startTime = System.nanoTime();
-				compressor.compress(inputDir, outputDir, fileSize);
-				System.out.println("Compression finished in " + (System.nanoTime() - startTime) / 1000000 + " ms");
-				System.out
-						.println("Maximum memory used (MB):" + FileUtils.bytesToMB(Runtime.getRuntime().totalMemory()));
-				break;
-			default:
-				break;
+				case ZIP:
+					Zip compressor = new Zip();
+					System.out.println("Starting to compress " + inputDir + " to " + outputDir + " using "
+							+ CompressionAlgo.ZIP.name() + " compression");
+					long startTime = System.nanoTime();
+					System.out.println("Start Time:::: "+ startTime);
+					compressor.compress(inputDir, outputDir, fileSize);
+					System.out.println("Compression finished in " + (System.nanoTime() - startTime) / 1000000 + " ms");
+					System.out
+							.println("Maximum memory used (MB):" + FileUtils.bytesToMB(Runtime.getRuntime().totalMemory()));
+					break;
+				default:
+					break;
 			}
-		} else if (args[0].equalsIgnoreCase(Commands.EXTRACT.name())) {
-			String inputDir = args[1];
-			String outputDir = args[2];
+		} else if (input==1) {
+			String inputDir = "/Users/ritwikgoel/Desktop/dir";
+			String outputDir = "/Users/ritwikgoel/Desktop";
 
 			// Currently using only ZIP extraction
 			CompressionAlgo extractionAlgorithm = CompressionAlgo.ZIP;
 			switch (extractionAlgorithm) {
-			case ZIP:
-				System.out.println("Starting to extract " + inputDir + " to " + outputDir + " using "
-						+ CompressionAlgo.ZIP.name() + " extraction");
-				long startTime = System.nanoTime();
-				Zip extractor = new Zip();
-				extractor.extract(inputDir, outputDir);
-				System.out.println("Extraction finished in " + (System.nanoTime() - startTime) / 1000000 + " ms");
-				System.out
-						.println("Maximum memory used (MB):" + FileUtils.bytesToMB(Runtime.getRuntime().totalMemory()));
-				break;
-			default:
-				break;
+				case ZIP:
+					System.out.println("Starting to extract " + inputDir + " to " + outputDir + " using "
+							+ CompressionAlgo.ZIP.name() + " extraction");
+					long startTime = System.nanoTime();
+					System.out.println("Start Time:::: "+ startTime);
+					Zip extractor = new Zip();
+					extractor.extract(inputDir, outputDir);
+					System.out.println("Extraction finished in " + (System.nanoTime() - startTime) / 1000000 + " ms");
+					System.out
+							.println("Maximum memory used (MB):" + FileUtils.bytesToMB(Runtime.getRuntime().totalMemory()));
+					break;
+				default:
+					break;
 
 			}
 		}
 
 	}
-
 }
